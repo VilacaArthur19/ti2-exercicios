00ae470af4119a0e4d9f7659be94feda0ae365e4
arthu <arthu@PCzin>
2026-03-06 13:53:22 -0300

    Adicionando arquivos fonte .java.

---
+++ exercicio1/src/exercicio1/Usuario.java
@@ -0,0 +1,37 @@
+package exercicio1;
+
+public class Usuario {
+	int codigo;
+	String login;
+	String senha;
+	char sexo;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	public char getSexo() {
+		return sexo;
+	}
+	public void setSexo(char sexo) {
+		this.sexo = sexo;
+	}
+	@Override
+	public String toString() {
+		return "Usuario [codigo=" + codigo + ", login=" + login + ", senha=" + senha + ", sexo=" + sexo + "]";
+	}
+
+}
